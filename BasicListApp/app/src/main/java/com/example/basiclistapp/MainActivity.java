e595a868e1261b0cd08f76c2274a6eac1a7095c7
Renz Jordan Tolentino <renzjordan1@gmail.com>
2019-11-15 14:18:56 -0500

    Finished tutorial on List View App

--- BasicListApp/app/src/main/java/com/example/basiclistapp/MainActivity.java
+++ BasicListApp/app/src/main/java/com/example/basiclistapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.basiclistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,7 +28,17 @@ public class MainActivity extends AppCompatActivity {
         prices = res.getStringArray(R.array.prices);
         descriptions = res.getStringArray(R.array.descriptions);
 
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
+        myListView.setAdapter(itemAdapter);
 
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("com.example.basiclistapp.ITEM_INDEX", i);
+                startActivity(showDetailActivity);
+            }
+        });
 
 //        myListView.setAdapter(new ArrayAdapter<String>(this, R.layout.my_listview_detail, items));
 
